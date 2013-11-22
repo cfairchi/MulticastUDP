f2b99615e0f237588288f780c98567820a689da2
Chris <chris@csfweb.net>
2013-11-22 13:13:16 -0500

    Update NetUtils.cs

--- NetUtils.cs
+++ NetUtils.cs
@@ -1,5 +1,9 @@
-namespace com.csf.netutils {
+namespace MulticastUDP {
     public static class NetUtils {
+        
+        // Returns first valid IP Address 
+        // If Multiple IP Addresses are assigned to host machine, additional code may be required to choose
+        // desired network interface
         public static IPAddress = getMyIPAddress() {
             IPHostEntry iphe = Dns.GetHostEntry(Dns.GetHostName());
             for(int i=0; i < iphe.AddressList.Length;++i){
